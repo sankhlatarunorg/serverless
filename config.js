f4facd44b1b963a4750751d2a6adc9d551912b74
tarunsankhla <47709623+tarunsankhla@users.noreply.github.com>
2024-03-31 18:11:40 -0400

    db

--- config.js
+++ config.js
@@ -1,10 +1,15 @@
 
 const {Sequelize,DataTypes} = require('sequelize');
 require('dotenv').config();
+console.log("DB_name:",process.env.DB_NAME );
+console.log("DB_USER:",process.env.DB_USER );
+console.log("DB_PASSWORD:",process.env.DB_PASSWORD );
+console.log("DB_HOST:",process.env.DB_HOST );
+
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME || 'user',
-    process.env.DB_USER || 'root',
+    process.env.DB_NAME || 'webapp',
+    process.env.DB_USER || 'webapp',
     process.env.DB_PASSWORD || '',
     {
         host: process.env.DB_HOST || 'localhost',
